2ad82422506ca89df7afecc86c04084fe1466149
장수제/201311240 <sujaes@naver.com>
2018-11-19 15:45:31 +0900

    modifying network errors

--- app/src/main/java/com/example/sujae/infoism/CrimeInfo.java
+++ app/src/main/java/com/example/sujae/infoism/CrimeInfo.java
@@ -2,88 +2,116 @@ package com.example.sujae.infoism;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.StrictMode;
+
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CrimeInfo extends Activity {
+    List<String> NM = new ArrayList<String>();
+    List<Double> XCODE = new ArrayList<Double>();
+    List<Double> YCODE = new ArrayList<Double>();
+
+    EditText edit;
+
+    static TextView text;
+    TextView text2;
+    String key="616d44704b776b6439366e78424248";
+    String data;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crimeinfo);
-        StrictMode.enableDefaults();
-        TextView status1 = (TextView)findViewById(R.id.text); //파싱된 결과확인!
-        boolean incontinent = false, inename = false, inname = false, innews = false, inwrtDt = false, initem=false;
-        String continent = null, ename = null, name = null, news = null, wrtDt = null;
-
-        try{
-            URL url = new URL("http://apis.data.go.kr/1262000/AccidentService/getAccidentList?"
-                    + "&isoCode1=CHN&isoCode2=IRQ&ServiceKey="
-                    + "qVtieL2r0pCsDUULqxPRLfwEFx5bPYvlq0hn3ioPYbB6CTPJbmEemyGT27f6eqPCW%2By%2FrCvigpanjyoXPYJ7Wg%3D%3D"
-            ); //검색 URL부분
-
-            XmlPullParserFactory parserCreator = XmlPullParserFactory.newInstance();
-            XmlPullParser parser = parserCreator.newPullParser();
-
-            parser.setInput(url.openStream(), null);
-
-            int parserEvent = parser.getEventType();
-            System.out.println("파싱시작합니다.");
-
-            while (parserEvent != XmlPullParser.END_DOCUMENT){
-                switch(parserEvent){
-                    case XmlPullParser.START_TAG://parser가 시작 태그를 만나면 실행
-                        if(parser.getName().equals("continent")){ //title 만나면 내용을 받을수 있게 하자
-                            incontinent = true;
-                        }
-                        if(parser.getName().equals("ename")){ //address 만나면 내용을 받을수 있게 하자
-                            inename = true;
-                        }
-                        if(parser.getName().equals("name")){ //mapx 만나면 내용을 받을수 있게 하자
-                            inname = true;
-                        }
-                        if(parser.getName().equals("news")){ //mapy 만나면 내용을 받을수 있게 하자
-                            innews = true;
-                        }
-                        if(parser.getName().equals("wrtDt")) { //mapy 만나면 내용을 받을수 있게 하자
-                            inwrtDt = true;
-
-                        }
-                        break;
-
-                    case XmlPullParser.TEXT://parser가 내용에 접근했을때
-                        if(incontinent){ //isTitle이 true일 때 태그의 내용을 저장.
-                            continent = parser.getText();
-                            incontinent = false;
-                        }
-                        if(inname){ //isAddress이 true일 때 태그의 내용을 저장.
-                            name = parser.getText();
-                            inname = false;
-                        }
-                        if(innews){ //isMapx이 true일 때 태그의 내용을 저장.
-                            news = parser.getText();
-                            innews = false;
-                        }
-                        if(inwrtDt){ //isMapy이 true일 때 태그의 내용을 저장.
-                            wrtDt = parser.getText();
-                            inwrtDt = false;
-                        }
-                        break;
-                    case XmlPullParser.END_TAG:
-                        if(parser.getName().equals("item")){
-                            status1.setText(status1.getText()+"대륙 : "+ continent +"\n 이름: "+ name +"\n 뉴스내용: " + news
-                                    +"\n 작성일 : " + wrtDt +"\n");
-                            initem=false;
-                        }
-                        break;
-                }
-                parserEvent = parser.next();
-            }
-        } catch(Exception e){
-            status1.setText("에러가..났습니다...");
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_crimeinfo);
+            edit= (EditText)findViewById(R.id.edit);
+            text= (TextView)findViewById(R.id.text);
+            text2= (TextView)findViewById(R.id.text2);
         }
+        // 버튼 하나를 통해 2개의 쓰레드를 돌려서 각각의 api의 데이터를 가져와 출력
+        public void mOnClick(View v){
+            switch( v.getId() ){
+                case R.id.button:
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+                            data= getXmlData();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    // TODO Auto-generated method stub
+                                    text.setText(data);
+                                }
+                            });
+                        }
+                    }).start();
+                    break;
+            }
+        }
+        String getXmlData() {
+            StringBuffer buffer = new StringBuffer();
+
+            String queryUrl = "http://openapi.seoul.go.kr:8088/" + key + "/xml/foodTruckInfo/1/50/";
+            try {
+                URL url = new URL(queryUrl); // 문자열로 된 요청 url을 URL 객체로 생성
+                InputStream is = url.openStream(); //url 위치로 인풋스트림 연결
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput(new InputStreamReader(is, "UTF-8"));
+                // inputstream 으로부터 xml 입력받기
+                String tag;
+                xpp.next();
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    switch (eventType) {
+                        case XmlPullParser.START_DOCUMENT:
+                            break;
+                        case XmlPullParser.START_TAG:
+                            tag = xpp.getName(); // 태그 이름 얻어오기
+                            if (tag.equals("foodTruckInfo")){
+
+                            }
+                            else if (tag.equals("NM")) {
+                                buffer.append("사업장명 : ");
+                                xpp.next();
+                                buffer.append(xpp.getText());
+                                // MSRSTENAME 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                                buffer.append("\n"); // 줄바꿈 문자 추가
+                            } else if (tag.equals("XCODE")) {
+                                buffer.append("X좌표 : ");
+                                xpp.next();
+                                buffer.append(xpp.getText());
+                                buffer.append("\n");
+                            } else if (tag.equals("YCODE")) {
+                                buffer.append("Y좌표 :");
+                                xpp.next();
+                                buffer.append(xpp.getText());//cpId
+                                buffer.append("\n");
+                            }
+                            break;
+                        case XmlPullParser.TEXT:
+                            break;
+                        case XmlPullParser.END_TAG:
+                            tag = xpp.getName(); // 태그 이름 얻어오기
+                            break;
+                    }
+                    eventType = xpp.next();
+
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch blocke.printStackTrace();
+            }
+            buffer.append("파싱 끝\n");
+            return buffer.toString();//StringBuffer 문자열 객체 반환
+        }
+
     }
-}
--- app/src/main/java/com/example/sujae/infoism/MainActivity.java
+++ app/src/main/java/com/example/sujae/infoism/MainActivity.java
@@ -3,9 +3,8 @@ package com.example.sujae.infoism;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,36 +15,125 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-    @Override
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+    String key="616d44704b776b6439366e78424248";
+    List<String> NM = new ArrayList<String>();
+    List<String> XCODE = new ArrayList<String>();
+    List<String> YCODE = new ArrayList<String>();
+    String temp="";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         FragmentManager fragmentManager = getFragmentManager();
-        MapFragment mapFragment = (MapFragment)fragmentManager
+        MapFragment mapFragment = (MapFragment) fragmentManager
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        String tempa  = getXmlData();
+        Toast.makeText(getApplicationContext(), tempa, Toast.LENGTH_LONG).show();
+
+        
+    }
+    public String getXmlData() {
+        StringBuffer buffer = new StringBuffer();
+        String queryUrl = "http://openapi.seoul.go.kr:8088/" + key + "/xml/foodTruckInfo/1/50/";
+        try {
+            URL url = new URL(queryUrl); // 문자열로 된 요청 url을 URL 객체로 생성
+            //여기까지는 받음
+            InputStream is = url.openStream(); //url 위치로 인풋스트림 연결
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new InputStreamReader(is, "UTF-8"));
+            // inputstream 으로부터 xml 입력받기
+
+            String tag;
+            xpp.next();
+            int eventType = xpp.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                Toast.makeText(getApplicationContext(), "파싱시작!! ", Toast.LENGTH_LONG).show();
+                switch (eventType) {
+                    case XmlPullParser.START_DOCUMENT:
+                        break;
+                    case XmlPullParser.START_TAG:
+                        tag = xpp.getName(); // 태그 이름 얻어오기
+                        if (tag.equals("foodTruckInfo")){
+                        }
+                        else if (tag.equals("NM")) {
+//                                buffer.append("사업장명 : ");
+                            xpp.next();
+                            temp = xpp.getText();
+//                            NM.add(temp);
+                            buffer.append(temp);
+                            // MSRSTENAME 요소의 TEXT 읽어와서 문자열버퍼에 추가
+//                                buffer.append("\n"); // 줄바꿈 문자 추가
+                        } else if (tag.equals("XCODE")) {
+//                                buffer.append("X좌표 : ");
+                            xpp.next();
+                            temp = xpp.getText();
+//                            XCODE.add(temp);
+                            buffer.append(temp);
+//                                buffer.append("\n");
+                        } else if (tag.equals("YCODE")) {
+//                                buffer.append("Y좌표 :");
+                            xpp.next();
+                            temp = xpp.getText();
+//                            YCODE.add(temp);
+                                buffer.append(temp);//cpId
+                                buffer.append("\n");
+                        }
+                        break;
+                    case XmlPullParser.TEXT:
+                        break;
+                    case XmlPullParser.END_TAG:
+                        tag = xpp.getName(); // 태그 이름 얻어오기
+                        break;
+                }
+                eventType = xpp.next();
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
+        }
+        buffer.append("파싱 끝\n");
+        return buffer.toString();//StringBuffer 문자열 객체 반환
     }
 
+    //맵에 알맞게 마커를 설정하고 마커 클릭시 알맞은 intent로 넘어감
     @Override
     public void onMapReady(final GoogleMap map) {
+        for (int i = 0; i < NM.size(); i++) {
+//            Toast.makeText(getApplicationContext(), "XCODE = "+XCODE.get(i), Toast.LENGTH_SHORT).show();
 
-        for(int idx = 0;idx<10; idx++){
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(new LatLng(37.632930+(idx/1000), 126.924490)).title("치치하우스가는길");
-            map.addMarker(markerOptions);
+//            MarkerOptions markerOptions = new MarkerOptions();
+//            markerOptions.position(new LatLng(Double.parseDouble(XCODE.get(i)),Double.parseDouble(YCODE.get(i)))).title(NM.get(i));
+//            map.addMarker(markerOptions);
+//            map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//                public boolean onMarkerClick(Marker marker) {
+//                    if (marker.getTitle().equals("스테이킹")) {
+//                        Intent intent1 = new Intent(getApplicationContext(), CrimeInfo.class);
+//                        startActivity(intent1);
+//                    }
+//                    return false;
+//                }
+//            });
         }
-        map.setOnMarkerClickListener(this);
-        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(37.632930, 126.924490)));
+//        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(Double.parseDouble(XCODE.get(0)),Double.parseDouble(YCODE.get(0)))));
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(new LatLng(30.3333,126.3333)).title("sasdasd");
+        map.addMarker(markerOptions);
+        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(30.3333,126.3333)));
 
         map.animateCamera(CameraUpdateFactory.zoomTo(10));
     }
-    public boolean onMarkerClick(Marker marker){
-        Intent intent1 = new Intent(getApplicationContext(), CrimeInfo.class);
-        startActivity(intent1);
-        return true;
-    }
+
 }
